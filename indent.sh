ba72dc6b79247d094894ea00fe9afb56f077b882
egouvea <egouvea@94700074-3cef-4d97-a70e-9c8c206c02f5>
2006-06-09 23:32:15 +0000

    Fixed a bug in fe_fft_real, in which spec[0] has to be treated as a special case, outside of the loop, this avoiding a reference to fft[fftsize], just beyond the allocated memory. Caveat: pocketsphinx not fixed. Also fixed handling of cases where aliasing occurs. Changed the indent.sh script to replace TAB with white space (but didn't run the script on the source files).
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinxbase@5603 94700074-3cef-4d97-a70e-9c8c206c02f5

--- indent.sh
+++ indent.sh
@@ -3,4 +3,4 @@
 # This is the equivalent of the following Emacs thing:
 # -*- tab-width: 8; c-file-style: "linux" -*-
 
-find . -name '*.c' -print0 | xargs indent -i8 -kr -psl -nce
+find . -name '*.c' -print0 | xargs indent -i8 -kr -psl -nce -nut
