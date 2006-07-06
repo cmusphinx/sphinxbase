eead1e9bb6454155f1e1289a9594c82d191db70a
dhdfu <dhdfu@94700074-3cef-4d97-a70e-9c8c206c02f5>
2006-07-06 12:56:07 +0000

    Whoops!  Fix the arguments to xargs to include -0 Also change the indentation to 4 spaces (flood of commits to follow)
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinxbase@5783 94700074-3cef-4d97-a70e-9c8c206c02f5

--- indent.sh
+++ indent.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # This is the equivalent of the following Emacs thing:
-# -*- tab-width: 8; c-file-style: "linux" -*-
+# -*- c-basic-offset: 4; indent-tabs-mode: nil -*-
 
-find . -name '*.c' -print0 | xargs indent -i8 -kr -psl -nce -nut
+find . -name '*.c' -print0 | xargs -0 indent -i4 -kr -psl -nce -nut
