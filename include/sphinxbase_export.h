4bc4f4e7da3c7483f6282d471b7323d5b1265f69
dhdfu <dhdfu@94700074-3cef-4d97-a70e-9c8c206c02f5>
2007-08-23 22:48:02 +0000

    This file is necessary
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinxbase@7197 94700074-3cef-4d97-a70e-9c8c206c02f5

---
+++ include/sphinxbase_export.h
@@ -0,0 +1,16 @@
+#ifndef __SPHINXBASE_EXPORT_H__
+#define __SPHINXBASE_EXPORT_H__
+
+/* Win32/WinCE DLL gunk */
+#if (defined(_WIN32) || defined(_WIN32_WCE)) \
+	&& !defined(LIBPOCKETSPHINX) && !defined(CYGWIN)
+#ifdef SPHINXBASE_EXPORTS
+#define SPHINXBASE_EXPORT __declspec(dllexport)
+#else
+#define SPHINXBASE_EXPORT __declspec(dllimport)
+#endif
+#else /* !_WIN32 */
+#define SPHINXBASE_EXPORT
+#endif
+
+#endif __SPHINXBASE_EXPORT_H__
