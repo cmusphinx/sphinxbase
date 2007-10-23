e883a0a5a6bc4c8784e7710918a72df915127853
dhdfu <dhdfu@94700074-3cef-4d97-a70e-9c8c206c02f5>
2007-10-23 16:27:59 +0000

    Reading/writing support for log tables (gosh they are big)
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinxbase@7322 94700074-3cef-4d97-a70e-9c8c206c02f5

--- src/libsphinxutil/logmath.c
+++ src/libsphinxutil/logmath.c
@@ -38,8 +38,12 @@
 #include "logmath.h"
 #include "err.h"
 #include "ckd_alloc.h"
+#include "mmio.h"
+#include "bio.h"
 
 #include <math.h>
+#include <string.h>
+#include <assert.h>
 
 struct logmath_s {
     float64 base;
@@ -51,6 +55,7 @@ struct logmath_s {
     uint8 shift;
     uint32 table_size;
     void *table;
+    mmio_file_t *filemap;
 };
 
 logmath_t *
@@ -135,14 +140,167 @@ logmath_init(float64 base, int width, int shift)
 }
 
 logmath_t *
-logmath_read(const char *filename)
+logmath_read(const char *file_name)
 {
+    logmath_t *lmath;
+    char **argname, **argval;
+    int32 byteswap, i;
+    int chksum_present, do_mmap;
+    uint32 chksum;
+    long pos;
+    FILE *fp;
+
+    E_INFO("Reading log table file '%s'\n", file_name);
+    if ((fp = fopen(file_name, "rb")) == NULL) {
+        E_ERROR("fopen(%s,rb) failed\n", file_name);
+        return NULL;
+    }
+
+    /* Read header, including argument-value info and 32-bit byteorder magic */
+    if (bio_readhdr(fp, &argname, &argval, &byteswap) < 0) {
+        E_ERROR("bio_readhdr(%s) failed\n", file_name);
+        fclose(fp);
+        return NULL;
+    }
+
+    lmath = ckd_calloc(1, sizeof(*lmath));
+    /* Default values. */
+    lmath->shift = 0;
+    lmath->width = 2;
+    lmath->base = 1.0001;
+
+    /* Parse argument-value list */
+    chksum_present = 0;
+    for (i = 0; argname[i]; i++) {
+        if (strcmp(argname[i], "version") == 0) {
+        }
+        else if (strcmp(argname[i], "chksum0") == 0) {
+            if (strcmp(argval[i], "yes") == 0)
+                chksum_present = 1;
+        }
+        else if (strcmp(argname[i], "width") == 0) {
+            lmath->width = atoi(argval[i]);
+        }
+        else if (strcmp(argname[i], "shift") == 0) {
+            lmath->shift = atoi(argval[i]);
+        }
+        else if (strcmp(argname[i], "logbase") == 0) {
+            lmath->base = atof(argval[i]);
+        }
+    }
+    bio_hdrarg_free(argname, argval);
+    chksum = 0;
+
+    /* Set up various necessary constants. */
+    lmath->log_of_base = log(lmath->base);
+    lmath->log10_of_base = log10(lmath->base);
+    lmath->inv_log_of_base = 1.0/lmath->log_of_base;
+    lmath->inv_log10_of_base = 1.0/lmath->log10_of_base;
+
+
+    /* #Values to follow */
+    if (bio_fread(&lmath->table_size, sizeof(int32), 1, fp, byteswap, &chksum) != 1) {
+        E_ERROR("fread(%s) (total #values) failed\n", file_name);
+        goto error_out;
+    }
+
+    /* Check alignment constraints for memory mapping */
+    do_mmap = 1;
+    pos = ftell(fp);
+    if (pos & ((long)lmath->width - 1)) {
+        E_WARN("%s: Data start %ld is not aligned on %d-byte boundary, will not memory map\n",
+                  file_name, pos, lmath->width);
+        do_mmap = 0;
+    }
+    /* Check byte order for memory mapping */
+    if (byteswap) {
+        E_WARN("%s: Data is wrong-endian, will not memory map\n", file_name);
+        do_mmap = 0;
+    }
+
+    if (do_mmap) {
+        lmath->filemap = mmio_file_read(file_name);
+        lmath->table = (char *)mmio_file_ptr(lmath->filemap) + pos;
+    }
+    else {
+        lmath->table = ckd_calloc(lmath->table_size, lmath->width);
+        if (bio_fread(lmath->table, lmath->width, lmath->table_size,
+                      fp, byteswap, &chksum) != lmath->table_size) {
+            E_ERROR("fread(%s) (%d x %d bytes) failed\n",
+                    file_name, lmath->table_size, lmath->width);
+            goto error_out;
+        }
+        if (chksum_present)
+            bio_verify_chksum(fp, byteswap, chksum);
+
+        if (fread(&i, 1, 1, fp) == 1) {
+            E_ERROR("%s: More data than expected\n", file_name);
+            goto error_out;
+        }
+    }
+    fclose(fp);
+
+    return lmath;
+error_out:
+    logmath_free(lmath);
     return NULL;
 }
 
 int32
-logmath_write(logmath_t *lmath, const char *filename)
+logmath_write(logmath_t *lmath, const char *file_name)
 {
+    FILE *fp;
+    long pos;
+    uint32 chksum;
+
+    E_INFO("Writing log table file '%s'\n", file_name);
+    if ((fp = fopen(file_name, "wb")) == NULL) {
+        E_ERROR("fopen(%s,wb) failed\n", file_name);
+        return -1;
+    }
+
+    /* For whatever reason, we have to do this manually at the
+     * moment. */
+    fprintf(fp, "s3\nversion 1.0\nchksum0 yes\n");
+    fprintf(fp, "width %d\n", lmath->width);
+    fprintf(fp, "shift %d\n", lmath->shift);
+    fprintf(fp, "logbase %f\n", lmath->base);
+    /* Pad it out to ensure alignment. */
+    pos = ftell(fp) + strlen("endhdr\n");
+    if (pos & ((long)lmath->width - 1)) {
+        size_t align = lmath->width - (pos & ((long)lmath->width - 1));
+        assert(lmath->width <= 8);
+        fwrite("        " /* 8 spaces */, 1, align, fp);
+    }
+    fprintf(fp, "endhdr\n");
+
+    /* Now write the binary data. */
+    chksum = (uint32)BYTE_ORDER_MAGIC;
+    fwrite(&chksum, sizeof(uint32), 1, fp);
+    chksum = 0;
+    /* #Values to follow */
+    if (bio_fwrite(&lmath->table_size, sizeof(uint32),
+                   1, fp, 0, &chksum) != 1) {
+        E_ERROR("fwrite(%s) (total #values) failed\n", file_name);
+        goto error_out;
+    }
+
+    if (bio_fwrite(lmath->table, lmath->width, lmath->table_size,
+                   fp, 0, &chksum) != lmath->table_size) {
+        E_ERROR("fwrite(%s) (%d x %d bytes) failed\n",
+                file_name, lmath->table_size, lmath->width);
+        goto error_out;
+    }
+    if (bio_fwrite(&chksum, sizeof(uint32), 1, fp, 0, NULL) != 1) {
+        E_ERROR("fwrite(%s) checksum failed\n", file_name);
+        goto error_out;
+    }
+
+    fclose(fp);
+    return 0;
+
+error_out:
+    fclose(fp);
     return -1;
 }
 
@@ -151,7 +309,10 @@ logmath_free(logmath_t *lmath)
 {
     if (lmath == NULL)
         return;
-    ckd_free(lmath->table);
+    if (lmath->filemap)
+        mmio_file_unmap(lmath->filemap);
+    else
+        ckd_free(lmath->table);
     ckd_free(lmath);
 }
 
@@ -189,7 +350,7 @@ logmath_add(logmath_t *lmath, int logb_x, int logb_y)
     }
 
     if (d < 0 || d > lmath->table_size) {
-        E_ERROR("Overflow in logmath_add: d = %d\n", d);
+        E_WARN("Overflow in logmath_add: d = %d\n", d);
         return r;
     }
 
--- test/unit/test_logmath/test_log_int16.c
+++ test/unit/test_logmath/test_log_int16.c
@@ -6,6 +6,7 @@ int
 main(int argc, char *argv[])
 {
 	logmath_t *lmath;
+	int32 rv;
 
 	lmath = logmath_init(1.0001, 2, 0);
 	TEST_ASSERT(lmath);
@@ -15,6 +16,19 @@ main(int argc, char *argv[])
 				   logmath_log(lmath, 5e-48)), -1087377);
 	TEST_EQUAL_LOG(logmath_add(lmath, logmath_log(lmath, 1e-48),
 				   logmath_log(lmath, 42)), 37378);
+
+	rv = logmath_write(lmath, "tmp.logadd");
+	TEST_EQUAL(rv, 0);
+	logmath_free(lmath);
+	lmath = logmath_read("tmp.logadd");
+	TEST_ASSERT(lmath);
+	TEST_EQUAL_LOG(logmath_log(lmath, 1e-48), -1105296);
+	TEST_EQUAL_LOG(logmath_log(lmath, 42), 37378);
+	TEST_EQUAL_LOG(logmath_add(lmath, logmath_log(lmath, 1e-48),
+				   logmath_log(lmath, 5e-48)), -1087377);
+	TEST_EQUAL_LOG(logmath_add(lmath, logmath_log(lmath, 1e-48),
+				   logmath_log(lmath, 42)), 37378);
+	logmath_free(lmath);
 
 	return 0;
 }
