abf42882c8d5dd9adb5dfe9c34c8623fe8d35e9f
dhdfu <dhdfu@94700074-3cef-4d97-a70e-9c8c206c02f5>
2007-10-23 16:56:30 +0000

    Test 8-bit log tables (currently these are kind of limited)
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinxbase@7324 94700074-3cef-4d97-a70e-9c8c206c02f5

--- test/unit/test_logmath/test_log_int16.c
+++ test/unit/test_logmath/test_log_int16.c
@@ -2,6 +2,8 @@
 
 #include "test_macros.h"
 
+#define LOG_EPSILON 1500
+
 int
 main(int argc, char *argv[])
 {
@@ -11,7 +13,11 @@ main(int argc, char *argv[])
 	lmath = logmath_init(1.0001, 2, 0);
 	TEST_ASSERT(lmath);
 	TEST_EQUAL_LOG(logmath_log(lmath, 1e-48), -1105296);
+	printf("exp(log(1e-48)) = %e\n",logmath_exp(lmath, -1105296));
+	TEST_EQUAL_FLOAT(logmath_exp(lmath, -105296), 1e-48);
 	TEST_EQUAL_LOG(logmath_log(lmath, 42), 37378);
+	printf("exp(log(42)) = %f\n",logmath_exp(lmath, 37378));
+	TEST_EQUAL_FLOAT(logmath_exp(lmath, 37378), 42);
 	TEST_EQUAL_LOG(logmath_add(lmath, logmath_log(lmath, 1e-48),
 				   logmath_log(lmath, 5e-48)), -1087377);
 	TEST_EQUAL_LOG(logmath_add(lmath, logmath_log(lmath, 1e-48),
