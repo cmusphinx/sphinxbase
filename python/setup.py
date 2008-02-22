9627aff1289900be4e9961b4f935e5cfff551e42
dhdfu <dhdfu@94700074-3cef-4d97-a70e-9c8c206c02f5>
2008-02-22 19:15:33 +0000

    Use pkg-config, build _sphinxbase module where it can be found
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinxbase@7851 94700074-3cef-4d97-a70e-9c8c206c02f5

--- python/setup.py
+++ python/setup.py
@@ -1,13 +1,17 @@
 from distutils.core import setup, Extension
+import commands
 
-module = Extension('sphinxbase._sphinxbase',
-                   include_dirs = ['../include',
-                                   '../i386-linux/include',
-                                   # FIXME: Use pkg-config
-                                   '/usr/local/include/sphinxbase/',
-                                   ],
-                   libraries = ['sphinxbase'],
-                   sources = ['_sphinxbasemodule.c'])
+def pkgconfig(*packages, **kw):
+    flag_map = {'-I': 'include_dirs', '-L': 'library_dirs', '-l': 'libraries'}
+    for token in commands.getoutput("pkg-config --libs --cflags %s" % ' '.join(packages)).split():
+        kw.setdefault(flag_map.get(token[:2]), []).append(token[2:])
+    return kw
+
+module = Extension('_sphinxbase',
+                   sources=['_sphinxbasemodule.c'],
+                   **pkgconfig('sphinxbase',
+                               include_dirs=['../include'],
+                               libraries=['sphinxbase']))
 
 setup(name = 'SphinxBase',
       version = '0.1',
