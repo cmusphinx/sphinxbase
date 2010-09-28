d8fa0d1fdc5d5086117afc4215486b826dbbf5b2
nshmyrev <nshmyrev@94700074-3cef-4d97-a70e-9c8c206c02f5>
2010-09-28 21:10:40 +0000

    Update for symbian compilation
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinxbase@10370 94700074-3cef-4d97-a70e-9c8c206c02f5

--- src/libsphinxbase/util/mmio.c
+++ src/libsphinxbase/util/mmio.c
@@ -48,22 +48,22 @@
 #include <stdlib.h>
 
 #ifdef GNUWINCE
-#  include <sys/wcebase.h>
-#  include <sys/wcetypes.h>
-#  include <sys/wcememory.h>
-#  include <sys/wcefile.h>
+# include <sys/wcebase.h>
+# include <sys/wcetypes.h>
+# include <sys/wcememory.h>
+# include <sys/wcefile.h>
+#elif defined(__SYMBIAN32__) /* SYMBIAN32 must be before WIN32 since Symbian SDK defines WIN32 as well */
+# include <unistd.h>
+# include <fcntl.h>
+# include <sys/stat.h>
+# include <sys/mman.h>
 #elif defined(_WIN32)
-#  include <windows.h>
-#elif defined(__ADSPBLACKFIN__) && !defined(__linux__)
+# include <windows.h>
+#elif defined(__ADSPBLACKFIN__) || defined(__linux__)
 # include <unistd.h>
 # include <fcntl.h>
 # include <sys/stat.h>
 # include <sys/file.h>
-# include <sys/mman.h>
-#elif !defined(__SYMBIAN32__)
-# include <unistd.h>
-# include <fcntl.h>
-# include <sys/stat.h>
 # include <sys/mman.h>
 #endif
 
