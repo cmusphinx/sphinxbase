b447feda41c982c037728c966e50729ac611e18c
dhdfu <dhdfu@94700074-3cef-4d97-a70e-9c8c206c02f5>
2010-10-12 00:46:38 +0000

    export huff code functions
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinxbase@10386 94700074-3cef-4d97-a70e-9c8c206c02f5

--- include/sphinxbase/huff_code.h
+++ include/sphinxbase/huff_code.h
@@ -50,6 +50,7 @@
 
 #include <stdio.h>
 
+#include <sphinxbase/sphinxbase_export.h>
 #include <sphinxbase/prim_type.h>
 #include <sphinxbase/cmd_ln.h>
 
@@ -58,61 +59,73 @@ typedef struct huff_code_s huff_code_t;
 /**
  * Create a codebook from 32-bit integer data.
  */
+SPHINXBASE_EXPORT
 huff_code_t *huff_code_build_int(int32 const *values, int32 const *frequencies, int nvals);
 
 /**
  * Create a codebook from string data.
  */
+SPHINXBASE_EXPORT
 huff_code_t *huff_code_build_str(char * const *values, int32 const *frequencies, int nvals);
 
 /**
  * Read a codebook from a file.
  */
+SPHINXBASE_EXPORT
 huff_code_t *huff_code_read(FILE *infh);
 
 /**
  * Write a codebook to a file.
  */
+SPHINXBASE_EXPORT
 int huff_code_write(huff_code_t *hc, FILE *outfh);
 
 /**
  * Print a codebook to a file as text (for debugging)
  */
+SPHINXBASE_EXPORT
 int huff_code_dump(huff_code_t *hc, FILE *dumpfh);
 
 /**
  * Retain a pointer to a Huffman codec object.
  */
+SPHINXBASE_EXPORT
 huff_code_t *huff_code_retain(huff_code_t *hc);
 
 /**
  * Release a pointer to a Huffman codec object.
  */
+SPHINXBASE_EXPORT
 int huff_code_free(huff_code_t *hc);
 
 /**
  * Attach a Huffman codec to a file handle for input/output.
  */
+SPHINXBASE_EXPORT
 FILE *huff_code_attach(huff_code_t *hc, FILE *fh, char const *mode);
 
 /**
  * Detach a Huffman codec from its file handle.
  */
+SPHINXBASE_EXPORT
 FILE *huff_code_detach(huff_code_t *hc);
 
 /**
  * Encode an integer, writing it to the file handle, if any.
  */
+SPHINXBASE_EXPORT
 int huff_code_encode_int(huff_code_t *hc, int32 sym, uint32 *outcw);
 
 /**
  * Encode a string, writing it to the file handle, if any.
  */
+SPHINXBASE_EXPORT
 int huff_code_encode_str(huff_code_t *hc, char const *sym, uint32 *outcw);
 
 /**
  * Decode an integer, reading it from the file if no data given.
  */
+SPHINXBASE_EXPORT
 int huff_code_decode_int(huff_code_t *hc, int *outval,
                          char const **inout_data,
                          size_t *inout_data_len,
@@ -121,6 +134,7 @@ int huff_code_decode_int(huff_code_t *hc, int *outval,
 /**
  * Decode a string, reading it from the file if no data given.
  */
+SPHINXBASE_EXPORT
 char const *huff_code_decode_str(huff_code_t *hc,
                                  char const **inout_data,
                                  size_t *inout_data_len,
