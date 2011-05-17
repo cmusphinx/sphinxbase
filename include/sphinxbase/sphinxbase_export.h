2780ec175afc5ff365ebe3788d9da5dc904d5389
nshmyrev <nshmyrev@94700074-3cef-4d97-a70e-9c8c206c02f5>
2011-05-17 13:01:30 +0000

    Unifies exports header with pocketsphinx
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinxbase@10969 94700074-3cef-4d97-a70e-9c8c206c02f5

--- include/sphinxbase/sphinxbase_export.h
+++ include/sphinxbase/sphinxbase_export.h
@@ -2,11 +2,9 @@
 #define __SPHINXBASE_EXPORT_H__
 
 /* Win32/WinCE DLL gunk */
-#if (defined(_WIN32) || defined(_WIN32_WCE)) && !defined(__SYMBIAN32__)
+#if (defined(_WIN32) || defined(_WIN32_WCE)) && !defined(__CYGWIN__) && !defined(__WINSCW__) && !defined(__SYMBIAN32__)
 #if defined(SPHINXBASE_EXPORTS) /* Visual Studio */
 #define SPHINXBASE_EXPORT __declspec(dllexport)
-#elif defined(__CYGWIN__) /* Disable this on Cygwin, it doesn't work */
-#define SPHINXBASE_EXPORT
 #else
 #define SPHINXBASE_EXPORT __declspec(dllimport)
 #endif
