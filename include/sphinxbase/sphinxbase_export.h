7bd4b6f686897269d4373696d153b024684a8811
nshmyrev <nshmyrev@94700074-3cef-4d97-a70e-9c8c206c02f5>
2016-01-22 21:10:33 +0000

    Introduce SPHINX_DLL macros to enable DLL compilation. Compile without exports otherwise.
    See for details https://sourceforge.net/p/cmusphinx/bugs/414
    
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/cmusphinx/code/trunk/sphinxbase@13161 94700074-3cef-4d97-a70e-9c8c206c02f5

--- include/sphinxbase/sphinxbase_export.h
+++ include/sphinxbase/sphinxbase_export.h
@@ -1,14 +1,14 @@
 #ifndef __SPHINXBASE_EXPORT_H__
 #define __SPHINXBASE_EXPORT_H__
 
-/* Win32/WinCE DLL gunk */
-#if (defined(_WIN32) || defined(_WIN32_WCE)) && !defined(_WIN32_WP) && !defined(__MINGW32__) && !defined(__CYGWIN__) && !defined(__WINSCW__) && !defined(__SYMBIAN32__)
-#if defined(SPHINXBASE_EXPORTS) /* Visual Studio */
+/* Win32 DLL gunk */
+#if defined(_WIN32) && defined(SPHINX_DLL)
+#if defined(SPHINXBASE_EXPORTS) /* DLL itself */
 #define SPHINXBASE_EXPORT __declspec(dllexport)
 #else
 #define SPHINXBASE_EXPORT __declspec(dllimport)
 #endif
-#else /* !_WIN32 */
+#else /* No DLL things*/
 #define SPHINXBASE_EXPORT
 #endif
 
