e1941dcc1ad3e6ed4d780bf0ee9912af5c3c3cca
syb0rg <darksyb0rg@gmail.com>
2016-08-04 16:54:10 -0500

    Fix compilation with OpenAL

--- src/libsphinxad/ad_openal.c
+++ src/libsphinxad/ad_openal.c
@@ -34,14 +34,13 @@
  * ====================================================================
  *
  */
-#include <config.h>
 #include <stdlib.h>
 #include <stdio.h>
 
 #include "ad.h"
 
-#include <al.h>
-#include <alc.h>
+#include <OpenAL/al.h>
+#include <OpenAL/alc.h>
 
 struct ad_rec_s {
     ALCdevice * device;
