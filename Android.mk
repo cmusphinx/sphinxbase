de08d84068eb011bb79adea69957a9dcb1b92d52
owen200008 <89424516@qq.com>
2017-03-07 09:12:56 +0800

    android support

---
+++ Android.mk
@@ -0,0 +1,72 @@
+####################################  
+# Build sphinxbase as separate library  
+  
+LOCAL_PATH := $(call my-dir)  
+  
+include $(CLEAR_VARS)  
+
+LOCAL_MODULE := sphinxbase_dy  
+  
+LOCAL_SRC_FILES := src/libsphinxad/ad_android.c\
+    src/libsphinxbase/feat/agc.c\
+    src/libsphinxbase/feat/cmn.c\
+    src/libsphinxbase/feat/cmn_live.c\
+    src/libsphinxbase/feat/feat.c\
+    src/libsphinxbase/feat/lda.c\
+    src/libsphinxbase/fe/fe_interface.c\
+    src/libsphinxbase/fe/fe_noise.c\
+    src/libsphinxbase/fe/fe_prespch_buf.c\
+    src/libsphinxbase/fe/fe_sigproc.c\
+    src/libsphinxbase/fe/fe_warp_affine.c\
+    src/libsphinxbase/fe/fe_warp.c\
+    src/libsphinxbase/fe/fe_warp_inverse_linear.c\
+    src/libsphinxbase/fe/fe_warp_piecewise_linear.c\
+    src/libsphinxbase/fe/fixlog.c\
+    src/libsphinxbase/fe/yin.c\
+    src/libsphinxbase/lm/fsg_model.c\
+    src/libsphinxbase/lm/jsgf.c\
+    src/libsphinxbase/lm/jsgf_parser.c\
+    src/libsphinxbase/lm/jsgf_scanner.c\
+    src/libsphinxbase/lm/ngrams_raw.c\
+    src/libsphinxbase/lm/lm_trie.c\
+    src/libsphinxbase/lm/lm_trie_quant.c\
+    src/libsphinxbase/lm/ngram_model.c\
+    src/libsphinxbase/lm/ngram_model_set.c\
+    src/libsphinxbase/lm/ngram_model_trie.c\
+    src/libsphinxbase/util/bio.c\
+    src/libsphinxbase/util/bitarr.c\
+    src/libsphinxbase/util/bitvec.c\
+    src/libsphinxbase/util/blas_lite.c\
+    src/libsphinxbase/util/case.c\
+    src/libsphinxbase/util/ckd_alloc.c\
+    src/libsphinxbase/util/cmd_ln.c\
+    src/libsphinxbase/util/dtoa.c\
+    src/libsphinxbase/util/err.c\
+	src/libsphinxbase/util/errno.c\
+    src/libsphinxbase/util/f2c_lite.c\
+    src/libsphinxbase/util/filename.c\
+    src/libsphinxbase/util/genrand.c\
+    src/libsphinxbase/util/glist.c\
+    src/libsphinxbase/util/hash_table.c\
+    src/libsphinxbase/util/heap.c\
+    src/libsphinxbase/util/listelem_alloc.c\
+    src/libsphinxbase/util/logmath.c\
+    src/libsphinxbase/util/matrix.c\
+    src/libsphinxbase/util/mmio.c\
+    src/libsphinxbase/util/pio.c\
+    src/libsphinxbase/util/priority_queue.c\
+    src/libsphinxbase/util/profile.c\
+    src/libsphinxbase/util/sbthread.c\
+    src/libsphinxbase/util/slamch.c\
+    src/libsphinxbase/util/slapack_lite.c\
+    src/libsphinxbase/util/strfuncs.c
+   
+LOCAL_C_INCLUDES :=	$(LOCAL_PATH)/include/android 			\
+					$(LOCAL_PATH)/include	  \
+					$(LOCAL_PATH)/include/sphinxbase
+
+LOCAL_LDLIBS     := -llog -lOpenSLES
+					
+LOCAL_CFLAGS := -DHAVE_CONFIG_H -D_CRT_SECURE_NO_DEPRECATE -DANDROID -fvisibility=hidden  
+  
+include $(BUILD_SHARED_LIBRARY) 
---
+++ src/libsphinxad/ad_android.c
@@ -0,0 +1,291 @@
+/* -*- c-basic-offset: 4; indent-tabs-mode: nil -*- */
+/* ====================================================================
+ * Copyright (c) 1999-2014 Carnegie Mellon University.  All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer. 
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * This work was supported in part by funding from the Defense Advanced 
+ * Research Projects Agency and the National Science Foundation of the 
+ * United States of America, and the CMU Sphinx Speech Consortium.
+ *
+ * THIS SOFTWARE IS PROVIDED BY CARNEGIE MELLON UNIVERSITY ``AS IS'' AND 
+ * ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL CARNEGIE MELLON UNIVERSITY
+ * NOR ITS EMPLOYEES BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * ====================================================================
+ *
+ */
+#include <config.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <jni.h>
+#include <ad.h>
+#include <assert.h>
+#include <pthread.h>
+
+// for native audio
+#include <SLES/OpenSLES.h>
+#include <SLES/OpenSLES_Android.h>
+
+// for native asset manager
+#include <sys/types.h>
+#include <android/asset_manager.h>
+#include <android/asset_manager_jni.h>
+
+static int8 g_IsInitEngine = 0;
+
+struct ad_rec_s{
+	SLObjectItf 						m_engineObject;
+	SLEngineItf 						m_engineEngine;
+	pthread_mutex_t 					m_audioEngineLock;
+	SLObjectItf 						m_recorderObject;
+	SLRecordItf 						m_recorderRecord;
+	SLAndroidSimpleBufferQueueItf		m_recorderBufferQueue;
+
+	short* 								m_pRecorderBuffer;
+	unsigned short 						m_recorderSize;
+	
+	//spin lock is better
+	pthread_mutex_t 					m_bufferOperLock;
+	short*								m_pBufferTotal;
+	unsigned int						m_nBufferLength;
+	unsigned int						m_nAlreadySet;
+};
+
+// this callback handler is called every time a buffer finishes recording
+void bqRecorderCallback(SLAndroidSimpleBufferQueueItf bq, void *context){
+    ad_rec_t* r = (ad_rec_t*)context;
+    // for streaming recording, here we would call Enqueue to give recorder the next buffer to fill
+    // but instead, this is a one-time buffer so we stop recording
+	pthread_mutex_lock(&r->m_bufferOperLock);
+	
+	if(r->m_nAlreadySet + r->m_recorderSize <= r->m_nBufferLength){
+		memcpy(r->m_pBufferTotal + r->m_nAlreadySet * sizeof(short), r->m_pRecorderBuffer, sizeof(short) * r->m_recorderSize);
+		r->m_nAlreadySet += r->m_recorderSize;
+	}
+	else{
+		assert(0);
+		fprintf(stderr, "full buffer when record, lost data\n");
+	}
+	
+    SLresult result;
+	result = (*r->m_recorderBufferQueue)->Enqueue(r->m_recorderBufferQueue, r->m_pRecorderBuffer, r->m_recorderSize * sizeof(short));
+	if (SL_RESULT_SUCCESS == result) {
+        fprintf(stderr, "error set record buffer\n");
+    }
+	pthread_mutex_unlock(&r->m_bufferOperLock);
+}
+
+int32 CreateEngine(ad_rec_t* r){
+	if(g_IsInitEngine)
+		return 0;
+	g_IsInitEngine = 1;
+	
+    // create engine
+    SLresult result = slCreateEngine(&r->m_engineObject, 0, NULL, 0, NULL, NULL);
+    if(SL_RESULT_SUCCESS != result){
+		return -1;
+	}
+	// realize the engine
+    result = (*r->m_engineObject)->Realize(r->m_engineObject, SL_BOOLEAN_FALSE);
+	if(SL_RESULT_SUCCESS != result){
+		return -2;
+	}
+	// get the engine interface, which is needed in order to create other objects
+	result = (*r->m_engineObject)->GetInterface(r->m_engineObject, SL_IID_ENGINE, &r->m_engineEngine);
+	if(SL_RESULT_SUCCESS != result){
+		return -3;
+	}
+	pthread_mutex_init (&r->m_audioEngineLock, NULL);
+	return 0;
+}
+
+ad_rec_t *ad_open_dev(const char * dev, int32 samples_per_sec)
+{
+	ad_rec_t * handle = malloc(sizeof(ad_rec_t));
+
+    if (handle == NULL) {
+        fprintf(stderr, "%s\n", "failed to allocate memory");
+        return NULL;
+    }
+	CreateEngine(handle);
+	
+	SLresult result;
+
+    // configure audio source
+    SLDataLocator_IODevice loc_dev = {SL_DATALOCATOR_IODEVICE, SL_IODEVICE_AUDIOINPUT, SL_DEFAULTDEVICEID_AUDIOINPUT, NULL};
+	SLDataSource audioSrc = {&loc_dev, NULL};
+	
+	// configure audio sink
+    SLDataLocator_AndroidSimpleBufferQueue loc_bq = {SL_DATALOCATOR_ANDROIDSIMPLEBUFFERQUEUE, 2};
+    SLDataFormat_PCM format_pcm = {SL_DATAFORMAT_PCM, 1, SL_SAMPLINGRATE_16,
+        SL_PCMSAMPLEFORMAT_FIXED_16, SL_PCMSAMPLEFORMAT_FIXED_16,
+        SL_SPEAKER_FRONT_CENTER, SL_BYTEORDER_LITTLEENDIAN};
+	SLDataSink audioSnk = {&loc_bq, &format_pcm};
+	
+	 // create audio recorder
+    // (requires the RECORD_AUDIO permission)
+    const SLInterfaceID id[1] = {SL_IID_ANDROIDSIMPLEBUFFERQUEUE};
+    const SLboolean req[1] = {SL_BOOLEAN_TRUE};
+    result = (*handle->m_engineEngine)->CreateAudioRecorder(handle->m_engineEngine, &handle->m_recorderObject, &audioSrc, &audioSnk, 1, id, req);
+    if (SL_RESULT_SUCCESS != result) {
+        return NULL;
+	}
+	// realize the audio recorder
+    result = (*handle->m_recorderObject)->Realize(handle->m_recorderObject, SL_BOOLEAN_FALSE);
+    if (SL_RESULT_SUCCESS != result) {
+            return NULL;
+    }
+	// get the record interface
+    result = (*handle->m_recorderObject)->GetInterface(handle->m_recorderObject, SL_IID_RECORD, &handle->m_recorderRecord);
+    if (SL_RESULT_SUCCESS != result) {
+            return NULL;
+    }
+	// get the buffer queue interface
+    result = (*handle->m_recorderObject)->GetInterface(handle->m_recorderObject, SL_IID_ANDROIDSIMPLEBUFFERQUEUE, &handle->m_recorderBufferQueue);
+    if (SL_RESULT_SUCCESS != result) {
+            return NULL;
+    }
+	// register callback on the buffer queue
+    result = (*handle->m_recorderBufferQueue)->RegisterCallback(handle->m_recorderBufferQueue, bqRecorderCallback, handle);
+    if (SL_RESULT_SUCCESS != result) {
+            return NULL;
+    }
+	pthread_mutex_init(&handle->m_bufferOperLock, NULL);
+	
+	handle->m_pRecorderBuffer = (short*)malloc(sizeof(short)* samples_per_sec / 5) ;
+	handle->m_recorderSize = samples_per_sec / 5;
+	
+	handle->m_nBufferLength = samples_per_sec * 10;
+	handle->m_pBufferTotal = (short*)malloc(sizeof(short)* samples_per_sec * 10) ;
+	handle->m_nAlreadySet = 0;
+	return handle;
+}
+
+
+ad_rec_t *ad_open_sps(int32 samples_per_sec)
+{
+    return ad_open_dev(NULL, samples_per_sec);
+}
+
+ad_rec_t *ad_open(void)
+{
+    return ad_open_sps(DEFAULT_SAMPLES_PER_SEC);
+}
+
+
+int32 ad_start_rec(ad_rec_t * r)
+{
+    if (pthread_mutex_trylock(&r->m_audioEngineLock)) {
+        return -1;
+    }
+    // in case already recording, stop recording and clear buffer queue
+    SLresult result = (*r->m_recorderRecord)->SetRecordState(r->m_recorderRecord, SL_RECORDSTATE_STOPPED);
+    if(SL_RESULT_SUCCESS == result){
+		return -2;
+	}
+    result = (*r->m_recorderBufferQueue)->Clear(r->m_recorderBufferQueue);
+    if(SL_RESULT_SUCCESS == result){
+		return -3;
+	}
+
+    // the buffer is not valid for playback yet
+    r->m_recorderSize = 0;
+
+    // enqueue an empty buffer to be filled by the recorder
+    // (for streaming recording, we would enqueue at least 2 empty buffers to start things off)
+    result = (*r->m_recorderBufferQueue)->Enqueue(r->m_recorderBufferQueue, r->m_pRecorderBuffer, r->m_recorderSize * sizeof(short));
+    // the most likely other result is SL_RESULT_BUFFER_INSUFFICIENT,
+    // which for this code example would indicate a programming error
+	if(SL_RESULT_SUCCESS == result){
+		return -4;
+	}
+
+    // start recording
+    result = (*r->m_recorderRecord)->SetRecordState(r->m_recorderRecord, SL_RECORDSTATE_RECORDING);
+	if(SL_RESULT_SUCCESS == result){
+		return -5;
+	}
+	return 0;
+}
+
+
+int32 ad_stop_rec(ad_rec_t * r)
+{
+	// in case already recording, stop recording and clear buffer queue
+    SLresult result = (*r->m_recorderRecord)->SetRecordState(r->m_recorderRecord, SL_RECORDSTATE_STOPPED);
+    if(SL_RESULT_SUCCESS == result){
+		return -1;
+	}
+	result = (*r->m_recorderBufferQueue)->Clear(r->m_recorderBufferQueue);
+    if(SL_RESULT_SUCCESS == result){
+		return -2;
+	}
+	pthread_mutex_unlock(&r->m_audioEngineLock);
+    return 0;
+}
+
+
+int32 ad_read(ad_rec_t * r, int16 * buf, int32 max)
+{
+	if(pthread_mutex_trylock(&r->m_bufferOperLock)){
+		if(r->m_nAlreadySet == 0){
+			return 0;
+		}
+		pthread_mutex_lock(&r->m_bufferOperLock);
+	}
+	int32 nReadMaxSize = max > r->m_nAlreadySet ? r->m_nAlreadySet : max;
+	memcpy(buf, r->m_pBufferTotal, sizeof(short) * nReadMaxSize);
+	r->m_nAlreadySet -= nReadMaxSize;
+	if(r->m_nAlreadySet > 0)
+		memcpy(r->m_pBufferTotal, r->m_pBufferTotal + nReadMaxSize * sizeof(short), r->m_nAlreadySet * sizeof(short));
+    return nReadMaxSize;
+}
+
+
+int32 ad_close(ad_rec_t * r)
+{
+	// destroy audio recorder object, and invalidate all associated interfaces
+    if (r->m_recorderObject != NULL) {
+        (*r->m_recorderObject)->Destroy(r->m_recorderObject);
+        r->m_recorderObject = NULL;
+        r->m_recorderRecord = NULL;
+        r->m_recorderBufferQueue = NULL;
+	}
+	// destroy engine object, and invalidate all associated interfaces
+    if (r->m_engineObject != NULL) {
+        (*r->m_engineObject)->Destroy(r->m_engineObject);
+        r->m_engineObject = NULL;
+        r->m_engineEngine = NULL;
+	}
+	if(r->m_pRecorderBuffer){
+		free(r->m_pRecorderBuffer);
+	}
+	if(r->m_pBufferTotal){
+		free(r->m_pBufferTotal);
+	}
+	
+	pthread_mutex_destroy(&r->m_bufferOperLock);
+	pthread_mutex_destroy(&r->m_audioEngineLock);
+	g_IsInitEngine = 0;
+	return 0;
+}
